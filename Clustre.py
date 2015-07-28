cab63b79f43d2bd64f2ea3905844a9b5e33e5c9a
Niels Anders <anders.niels@gmail.com>
2015-07-28 11:58:17 +0200

    added readme

--- Clustre.py
+++ Clustre.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-CLustre: semi-automated lineament clustering for palaeo-glacial reconstruction
+# CLustre: semi-automated lineament clustering for palaeo-glacial reconstruction
 -------------
+
+This script accompanies the paper of Smith et al. (In Press) in ESPL. Here we present a semi-automated algorithm, CLustre, for lineament clustering that uses a locally adaptive, region growing, methodology. CLustre is demonstrated with a polyline data set (ESRI shapefile) representing glacial landforms for palaeo-glacial reconstruction. 
+
+NOTE: This script is accompanied by example data from Stokes et al. 2003. When used please cite: 
+Stokes, C.R. & Clark, C.D. (2003). The Dubawnt Lake palaeo-ice stream: evidence for dynamic ice sheet behaviour on the Canadian Shield and insights regarding the controls on ice-stream location and vigour. Boreas 32: 263-279. doi: 10.1111/j.1502-3885.2003.tb01442.x
+
 Input: shapefile
 Output: edited shapefile with new attribute fields and values
-
 Usage: run Clustre.py with arguments from bash/terminal 
+
 -------------
 Arguments:
 * lines='location/to/shapefile.shp'
@@ -17,9 +23,9 @@ Arguments:
 * plotYN='yes' or 'no' to plot results in preview.png
 
 example:
-in linux:   python Clustre.py lines=\'example_data/example.shp\' th_distance=5000 th_orient=5 th_length=5000 label=1 seed_ID=2511 plotYN=\'yes\'
-in windows: python Clustre.py lines='example_data/example.shp' th_distance=5000 th_orient=5 th_length=5000 label=1 seed_ID=2511 plotYN='yes'
 
+in linux: python Clustre.py lines=\'example/dubawnt.shp\' th_distance=10000 th_orient=2 th_length=3000 label=1 seed_ID=5175 plotYN=\'yes\'
+in windows: python Clustre.py lines='example/dubawnt.shp' th_distance=10000 th_orient=2 th_length=3000 label=1 seed_ID=5715 plotYN='yes'
 
 -------------
 dependencies (and tested version numbers in brackets): 
@@ -32,7 +38,7 @@ Details are described in Smith MJ, Anders NS, Keesstra SD, In Press. CLustre: se
 
 For further questions please contact Niels Anders: n.s.anders@uva.nl
 
-(c) Niels Anders
+(c) Niels Anders, Mike Smith
 Computational Geo-Ecology, Institute for Biodiversity and Ecosystem Dynamics
 University of Amsterdam, The Netherlands
 """
